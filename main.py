5b0ea01d9edb6f5a00cfa9087f64bf43ef0dd1ef
CRUD004 <lhb423@naver.com>
2021-02-07 19:36:00 +0900

    매매입체별 분석 코드
    api 옵션 가이드:
    https://money2.creontrade.com/e5/mboard/ptype_basic/HTS_Plus_Helper/DW_Basic_Read_Page.aspx?boardseq=284&seq=242&page=1&searchString=7254&p=8841&v=8643&m=9505
    
    최고가 필요시 아래 주석의 Stock chart 모듈에서 가져올 수 있음
    https://money2.creontrade.com/e5/mboard/ptype_basic/HTS_Plus_Helper/DW_Basic_Read_Page.aspx?boardseq=284&seq=102&page=1&searchString=%EC%B0%A8%ED%8A%B8&p=8841&v=8643&m=9505

--- main.py
+++ main.py
@@ -1,2 +1,55 @@
+import sys
+import win32com.client
+import os
 
-print("Project: Golden Goose")
+
+objRq = win32com.client.Dispatch("CpSysDib.CpSvr7254")
+objRq.SetInputValue(0, 'A000270') #string 종목코드
+objRq.SetInputValue(1, 0) #short 기간선택 0:사용자지정, 1~3:개월 4:6개월
+objRq.SetInputValue(2, 20200301) #long 시작일자
+objRq.SetInputValue(3, 20210205) #long 종료일자
+objRq.SetInputValue(4, ord('0')) #char 매매비중 0:순매수 1:매매비중
+objRq.SetInputValue(5, 0) #short 투자자
+objRq.SetInputValue(6, ord('1')) #char 데이터 0: 순매수량 1: 금액
+
+while True:
+    objRq.BlockRequest()
+    count = objRq.GetHeaderValue(1)
+
+    if count == 0:
+        print("no more data")
+        break
+    
+    print("종목코드: ", objRq.GetHeaderValue(0))
+    print("총 데이터: ", count)
+    print("시작일자: ", objRq.GetHeaderValue(2))
+    print("종료일자: ", objRq.GetHeaderValue(3))
+
+    for i in range(count):
+        print("date:{} 개인: {} 기관: {} 외국인: {}".format(objRq.GetDataValue(0,i), objRq.GetDataValue(1,i), objRq.GetDataValue(2,i), objRq.GetDataValue(3,i)))
+
+''' 
+#종목의 일자별 정보를 가져올 수 있음
+objRq = win32com.client.Dispatch("CpSysDib.StockChart")
+
+objRq.SetInputValue(0, 'A000270')  # string 종목코드
+objRq.SetInputValue(1, ord('1'))  # char '1': 기간, '2': 갯수
+objRq.SetInputValue(3, 20210101) #ulong 요청시작일
+#objRq.SetInputValue(2, 20210206) #ulong 요청종료일
+objRq.SetInputValue(4, 10) #ulong 요청갯수
+
+#0: 날짜
+#5: 종가
+objRq.SetInputValue(5, [0,5,20,21]) #long list 필드 배열
+
+objRq.BlockRequest()
+
+print("종목코드: ", objRq.GetHeaderValue(0))
+print("필드: ", objRq.GetHeaderValue(2))
+cnt = objRq.GetHeaderValue(3)
+print("수신개수: ", cnt)
+
+for i in range(cnt-1, 0,-1):
+    #print("date:{} 종가: {} ".format(objRq.GetDataValue(0,i), objRq.GetDataValue(1,i)))
+    print("date:{} 종가: {} 기관순매수: {} 기관누적순매수: {}".format(objRq.GetDataValue(0,i), objRq.GetDataValue(1,i), objRq.GetDataValue(2,i), objRq.GetDataValue(3,i)))
+'''
